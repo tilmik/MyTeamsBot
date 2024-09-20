b84230ce52d9afc84aa68a5b1dd894473044de67
Sourjya Bhaumik <tilmik@gmail.com>
2024-09-20 11:41:09 +0530

    Formatting fixes

--- src/workItemsCommandHandler.js
+++ src/workItemsCommandHandler.js
@@ -4,16 +4,16 @@ const { CardFactory, MessageFactory } = require("botbuilder");
 const workItemsFactory = require("./workItemsFactory")
 
 class WorkItemsCommandHandler {
-  triggerPatterns = "myWorkItems";
+    triggerPatterns = "myWorkItems";
 
-  async handleCommandReceived(context, message) {
-    // verify the command arguments which are received from the client if needed.
-    console.log(`App received message: ${message.text}`);
+    async handleCommandReceived(context, message) {
+        // verify the command arguments which are received from the client if needed.
+        console.log(`App received message: ${message.text}`);
 
-    let cardJson = workItemsFactory.buildCard(0);
-    //console.log('Sending: ', JSON.stringify(cardJson));
-    return MessageFactory.attachment(CardFactory.adaptiveCard(cardJson));
-  }
+        let cardJson = workItemsFactory.buildCard(0);
+        //console.log('Sending: ', JSON.stringify(cardJson));
+        return MessageFactory.attachment(CardFactory.adaptiveCard(cardJson));
+    }
 }
 
 module.exports = {
